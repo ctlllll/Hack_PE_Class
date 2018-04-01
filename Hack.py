18335cde4aa7d756d11432a86845d53e42dfa1b2
Tianle Cai <caitianle1998@pku.edu.cn>
2018-04-01 13:06:54 +0800

    Create Hack.py

---
+++ Hack.py
@@ -0,0 +1,7 @@
+TMP = input(
+    'Please input the distance(km) you want to add to the total distance:')
+print('Well now, simply dress up and go to Weiming Lake.')
+print('Don\'t forget to open your GPS and keep your APP on.')
+print('After running %d km, you will see the change.' % int(TMP))
+print('I hope you will not feel too tired for the distance you typed.')
+print('Happy April fool\'s day~')
